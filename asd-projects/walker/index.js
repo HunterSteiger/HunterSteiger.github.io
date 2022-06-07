92a4b2dffda4c813642828a670d1cf5f66e9c333
HunterSteiger <hunter.steiger2005@gmail.com>
2022-06-07 20:36:21 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -28,7 +28,7 @@ function runProgram(){
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -60,6 +60,20 @@ function runProgram(){
     }
 
   }
+  function handleKeyUp(event) {
+    if (event.which === KEY.ENTER){
+      console.log("enter pressed")
+    } if (event.which === KEY.RIGHT){
+      speedX = 0
+    } if (event.which === KEY.LEFT){
+      speedX = 0
+    } if (event.which === KEY.UP){
+      speedY = 0
+    } if (event.which === KEY.DOWN){
+      speedY = 0
+    }
+
+  }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
@@ -71,6 +85,7 @@ function runProgram(){
 
   function redrawGameItem(){
     $("#walker").css("left", positionX);
+    $("#walker").css("top", positionY);
   }
 
   function endGame() {
